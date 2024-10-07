dc987cd0d9d5fac08968b318f89a0cf14257746b
samuel.jimenez <samuel.jimenez@106-15.info2>
2024-10-07 13:21:37 +0200

    Ejercicios del 1 al 8

--- Tema2/src/parte1/Ejer8.java
+++ Tema2/src/parte1/Ejer8.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 public class Ejer8 {
 
 	public static void main(String[] args) {
-		float nota;
+		// Creacion variable numero que sera utilizada para declarar la nota del alumno
+
 		float numero;
+		// Creacion Scanner
 		Scanner lectura = new Scanner(System.in);
+		// Print que indica las ordenes al usuario
 		System.out.println("Introduce la nota del alumno");
+		// Declaramos el valor de numero en base al input del usuario
 		numero = lectura.nextFloat();
+		/*
+		 * Sucesion de condicionales que printearan la nota final del alumno. Si
+		 * numero<5 la nota sera insuficiente, si numero>=5 y numero<6 la nota sera
+		 * suficiente, si numero<=6 y numero<7 la nota sera bien si numero>=7 y numero<9
+		 * la nota sera notable y por ultimo si la nota>=9 y numero<=10 la nota sera
+		 * sobresaliente, si el numero introducido no se encuentra dentro del rango
+		 * [0,10] el programa indicara que se debe introducir una nota valida
+		 */
 		if (numero < 5)
 			System.out.println("La nota del alumno es: Insuficiente");
 		else if (numero >= 5 && numero < 6)
@@ -24,6 +36,8 @@ public class Ejer8 {
 			System.out.println("Introduzca una nota valida");
 		else
 			System.out.println("Introduzca una nota valida");
+		// Cierre Scanner
+		lectura.close();
 
 	}
 
--- Tema2/src/parte1/Ejer9.java
+++ Tema2/src/parte1/Ejer9.java
@@ -7,12 +7,12 @@ public class Ejer9 {
 	public static void main(String[] args) {
 		String eleccion1;
 		String eleccion2;
-		Scanner lectura=new Scanner(System.in);
-		eleccion1=lectura.nextLine();
-		eleccion2=lectura.nextLine();
-		if(eleccion1.equals(eleccion2))
+		Scanner lectura = new Scanner(System.in);
+		eleccion1 = lectura.nextLine();
+		eleccion2 = lectura.nextLine();
+		if (eleccion1.equals(eleccion2))
 			System.out.println("El resultado ha sido empate");
-	
+
 	}
 
 }
